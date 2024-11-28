e4eb3a8a20683c2e06e80aed7de9e1b86d08a997
Bennett <bascott1002@gmail.com>
2024-11-28 18:36:17 -0500

    Server gracefully shuts down

--- app.go
+++ app.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	server "hxgo-skeleton/server"
 
@@ -16,9 +20,28 @@ func main() {
 	host := os.Getenv("SERVER_HOST")
 	port := fmt.Sprintf(":%s", os.Getenv("SERVER_PORT"))
 	addr := fmt.Sprintf("http://%s%s", host, port)
+	routes := server.Routes()
+	server := &http.Server{
+		Addr:    port,
+		Handler: routes,
+	}
 
-	router := server.Routes()
+	go func() {
+		fmt.Printf("Listening on address: %s\n", addr)
+		server.ListenAndServe()
+	}()
 
-	fmt.Printf("Listening on address: %s\n", addr)
-	http.ListenAndServe(port, router)
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
+
+	<-signals
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		fmt.Printf("Server forced to shutdown: %v", err)
+	}
+
+	fmt.Println("\nServer gracefully shutdown")
 }

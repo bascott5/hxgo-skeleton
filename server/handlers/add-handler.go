8cbc2c03876c377440ffd2af63947023d3a66151
Bennett <bascott1002@gmail.com>
2024-11-27 23:52:32 -0500

    Created todo demo

---
+++ server/handlers/add-handler.go
@@ -0,0 +1,28 @@
+package handlers
+
+import (
+	"log"
+	"net/http"
+	"text/template"
+)
+
+func AddHandler() http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		err := r.ParseForm()
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		note := r.FormValue("note")
+
+		tmpl, err := template.New("item.tmpl").ParseFiles("client/views/hx-templates/item.tmpl")
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		err = tmpl.Execute(w, note)
+		if err != nil {
+			log.Fatalln(err)
+		}
+	})
+}
--- server/routes.go
+++ server/routes.go
@@ -12,7 +12,11 @@ func Routes() http.Handler {
 	fs := http.FileServer(http.Dir("client/static"))
 	mux.Handle("/static/", http.StripPrefix("/static/", fs))
 
+	// pages
 	mux.HandleFunc("/", handlers.IndexHandler())
+
+	// templates
+	mux.HandleFunc("/add", handlers.AddHandler())
 
 	return middleware.LoggerMiddleware(mux)
 }

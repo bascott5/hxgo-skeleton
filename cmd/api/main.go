8a721ddf8dc3f890f32cb0076a93bb9357c306bb
Bennett <bascott1002@gmail.com>
2024-12-14 21:27:00 -0500

    Improved organization

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,48 +1,9 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
-	"time"
-
 	server "hxgo-skeleton/internal/server"
-
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	godotenv.Load()
-
-	host := os.Getenv("SERVER_HOST")
-	port := fmt.Sprintf(":%s", os.Getenv("SERVER_PORT"))
-	addr := fmt.Sprintf("http://%s%s", host, port)
-	routes := server.Routes()
-	server := &http.Server{
-		Addr:    port,
-		Handler: routes,
-	}
-
-	go func() {
-		fmt.Printf("Listening on address: %s\n", addr)
-		server.ListenAndServe()
-	}()
-
-	signals := make(chan os.Signal, 1)
-	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
-
-	<-signals
-
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
-	err := server.Shutdown(ctx)
-	if err != nil {
-		fmt.Printf("Server forced to shutdown: %v", err)
-	}
-
-	fmt.Println("\nServer shutdown gracefully")
+	server.Server()
 }
